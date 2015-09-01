72215b952d03538bfce19491df8e491dfa120e94
David Lefever <dl@taktik.be>
2015-09-01 14:22:31 +0200

    [IMP] upgraded agent to 5.8.1.

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@ FROM ubuntu:14.04
 MAINTAINER dl@taktik.be
 
 ADD . /build
-RUN chmod +x /build/build.sh && /build/build.sh
+RUN chmod +x /build/build.sh && sleep 5 && /build/build.sh
 
 # Define additional metadata for our image.
 VOLUME /var/lib/docker
--- scripts/start_bamboo_agent.sh
+++ scripts/start_bamboo_agent.sh
@@ -2,4 +2,4 @@
 
 # TODO: use environment variable for the host
 
-java -jar /bamboo/atlassian-bamboo-agent-installer-5.5.0.jar https://bamboo.taktik.be/agentServer/
+java -jar /bamboo/atlassian-bamboo-agent-installer-5.8.1.jar https://bamboo.taktik.be/agentServer/
